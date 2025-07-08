0227bb6e09b77344c5f1c3b15d05f0012db8fd68
Juan Pablo Montoya Hernandez <juanrona12@gmail.com>
2025-07-08 13:45:42 -0600

    add: cors

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    cors: {
+      origin: "http://localhost:8081/",
+    },
+  },
   plugins: [react(), tailwindcss()],
 });
