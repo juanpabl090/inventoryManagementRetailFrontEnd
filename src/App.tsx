637c8079cf2ca96e3d522b30d73aa4490066e06e
Juan Pablo Montoya Hernandez <juanrona12@gmail.com>
2025-07-07 18:12:01 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <h1 className="h-1 text-amber-800">
+        hello, I'm using vite react ts with tailwindcss
+      </h1>
+    </>
+  );
+}
