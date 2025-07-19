ea3276aa2b45cf44073191cf9e3c7348e08fc54f
Juan Pablo Montoya Hernandez <juanrona12@gmail.com>
2025-07-18 21:08:48 -0600

    fix: compatibility versions of tailwindcss, rollback: version of tailwind 4.1 -> 3.7

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./styles/styles.css";
 
 export default function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-primary-800">
+    <div className="min-h-screen flex items-center justify-center bg-success-300">
       texto con colores personalizados
     </div>
   );
