05b6aa65e25d59667c624738c011bc04507af3f1
Juan Pablo Montoya Hernandez <juanrona12@gmail.com>
2025-07-22 11:25:03 -0600

    improve: responsive nav & leftmenu Components

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,12 @@ export default function App() {
         <Nav />
       </div>
       <div className="flex flex-1 overflow-hidden">
-        <div className="w-64 flex-shrink-0">
+        <div className="max-w-7xl flex-shrink-0">
           <LeftMenu />
         </div>
-        <main className="flex-1 p-4 overflow-auto bg-neutral-100">
-          <div className="flex flex-col justify-start">
-            <h1 className="text-neutral-950 text-base font-bold">Dashboard</h1>
+        <main className="flex flex-col p-4 overflow-auto bg-neutral-100">
+          <h1 className="text-neutral-950 text-base font-bold">Dashboard</h1>
+          <div className="flex-wrap xs:flex-row md:lg:flex md:lg:flex-col justify-start">
           </div>
         </main>
       </div>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,22 +2,20 @@ import { Store } from "lucide-react";
 
 export default function Nav() {
   return (
-    <>
-      <nav className="flex items-center bg-white h-16 m-0">
-        <div className="mx-64 flex items-center space-x-3">
-          <div className="h-10 w-10 rounded-xl bg-primary-600 flex items-center justify-center">
-            <Store className="h-6 w-6 text-white" />
-          </div>
-          <div className="flex flex-col leading-none">
-            <span className="text-[20px] font-semibold text-neutral-900">
-              Retail
-            </span>
-            <span className="text-[14px] text-neutral-500">
-              Sistema de Gestión de Inventario
-            </span>
-          </div>
+    <nav className="flex items-center bg-white h-16 shadow-sm w-full px-4 sm:px-6 lg:px-8">
+      <div className="flex items-center space-x-3 w-full max-w-7xl mx-auto">
+        <div className="h-10 w-10 rounded-xl bg-primary-600 flex-shrink-0 flex items-center justify-center">
+          <Store className="h-6 w-6 text-white" />
         </div>
-      </nav>
-    </>
+        <div className="flex flex-col leading-none">
+          <span className="text-lg sm:text-[20px] font-semibold text-neutral-900">
+            Retail
+          </span>
+          <span className="text-xs sm:text-[14px] text-neutral-500">
+            Sistema de Gestión de Inventario
+          </span>
+        </div>
+      </div>
+    </nav>
   );
 }
